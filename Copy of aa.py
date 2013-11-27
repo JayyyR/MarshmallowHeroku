37c8154516125316a8fe87d012c1886ac78b7490
Joe <flatpita92@gmail.com>
2013-11-26 20:47:46 -0500

    hasvoteds

--- Copy of aa.py
+++ Copy of aa.py
@@ -4,5 +4,6 @@ import datetime
 #kill
 hs = {'id': 'jayr'}
 print ("\n...voting Joe")
-getNear = requests.post('http://jayyyyrwerewolf.herokuapp.com/players/setVoted', params = hs)
+getNear = requests.get('http://jayyyyrwerewolf.herokuapp.com/players/hasVoted', params = hs)
 
+print getNear.text
