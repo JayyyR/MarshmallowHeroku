d5ee49d7bccbaedcd93adf91a9d10118c470cb25
Joe <flatpita92@gmail.com>
2013-12-14 16:23:57 -0500

    pw

--- Copy of aa.py
+++ Copy of aa.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 
 #kill
-hs = {'score': 1234}
+hs = {'score': "1234", 'name': "JOE"}
 print ("\n...voting Joe")
 getNear = requests.post('http://jayyyrmarshmallow.herokuapp.com/insertscore', params = hs)
 print getNear
